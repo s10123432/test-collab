660d2501820033420818fa579e1ab7e06cd8060a
Suroj <surojcodes@gmail.com>
2026-04-07 19:37:25 -0700

    add calculator

--- index.ts
+++ index.ts
@@ -2,3 +2,18 @@ export const sum = (a: number, b: number) => a + b;
 export const sub = (a: number, b: number) => a - b;
 export const mul = (a: number, b: number) => a * b;
 export const div = (a: number, b: number) => a / b;
+
+export const calculator = (a: number, b: number, operator: string): number => {
+  switch (operator) {
+    case "+":
+      return sum(a, b);
+    case "-":
+      return sub(a, b);
+    case "*":
+      return mul(a, b);
+    case "/":
+      return div(a, b);
+    default:
+      throw new Error(`Unknown operator: ${operator}`);
+  }
+};
